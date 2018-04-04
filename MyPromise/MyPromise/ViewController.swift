fbfaca60f95cd44f17e466a4fff8f97e4a2f51cc
Olga Saliy <olia.white.1996@gmail.com>
2018-04-04 11:05:12 +0300

    Create project

---
+++ MyPromise/MyPromise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyPromise
+//
+//  Created by Olha Salii on 4/4/18.
+//  Copyright © 2018 Olha Salii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
